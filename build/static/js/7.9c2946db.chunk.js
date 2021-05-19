5461c2403cb991c06f482353ed3769f472d32a45
swayambhu <swayambhu43@gmail.com>
2021-05-19 11:15:19 +0530

    Production Build

---
+++ build/static/js/7.9c2946db.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-quote"]=this["webpackJsonpreact-quote"]||[]).push([[7],{50:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:"Page not found!"})})}}}]);
+//# sourceMappingURL=7.9c2946db.chunk.js.map
